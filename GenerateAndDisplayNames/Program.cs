c4d0dc2fc9111cdba47739b4dfae2e36d4c53dec
Poetic_Programmer <michaelgeorgedunleavy@gmail.com>
2020-02-04 13:35:04 +0000

    First Build - uses array to generate a list of names and prints them

--- GenerateAndDisplayNames/Program.cs
+++ GenerateAndDisplayNames/Program.cs
@@ -10,6 +10,26 @@ namespace GenerateAndDisplayNames
     {
         static void Main(string[] args)
         {
+            string[] names = GenerateNames();
+            PrintNames(names);
+        }
+
+        static string[] GenerateNames()
+        {
+            string[] names = new string[4];
+            names[0] = "batman";
+            names[1] = "robin";
+            names[2] = "nightwing";
+            names[3] = "batwoman";
+            return names;
+        }
+
+        static void PrintNames(string[] names)
+        {
+            foreach(string s in names)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
